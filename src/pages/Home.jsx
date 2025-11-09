22de21829d17b96152cd4d095eead8e3af8e6a61
sooraj <soorajpsuresh74@gmail.com>
2025-11-09 22:44:17 +0530

    new machine

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,18 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/HeroSection";
 import Features from "../components/Features";
 import Mission from "../components/Mission";
-
+import RealEstateLanding from "../subui/sample_1"
 
 const Home = () => {
   return (
-  
-      
+    <>
+      {/* <div><RealEstateLanding></RealEstateLanding></div> */}
       <div className="flex-grow">
         <Hero />
-        <Mission/>
+        <Mission />
         <Features />
-    </div>
+      </div>
+    </>
   );
 };
 
